c87cdfb602cc88436d1631ec9a1383ef1fcac0ea
Tahsin Hossain <121660401+tahsinhossain123@users.noreply.github.com>
2024-01-16 23:48:07 -0500

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from 'next'
+import { Inter, Space_Grotesk } from 'next/font/google'
+import './globals.css'
+import Navbar from '@/components/Navbar'
+
+const inter = Inter({ subsets: ['latin'] })
+const SpaceGrotesk = Space_Grotesk({ 
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700']
+})
+
+export const metadata: Metadata = {
+  title: 'Pricewise',
+  description: 'Track proudct prcies effortlesslu and save money on your online shopping.' ,
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main className="max-w-10x1 mx-auto">
+          <Navbar />
+          {children}
+        </main>       
+      </body>
+    </html>
+  )
+}
